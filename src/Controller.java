2bc7c569de863b5f1123a240c75ffeeb8de4cf6c
Alina Christenbury <alina.christenbury@gmail.com>
2018-04-02 13:13:09 -0400

    changes to controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,24 +1,32 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 /**
  * Do not modify this file without permission from your TA.
  **/
 public class Controller {
-
+	
 	private Model model;
 	private View view;
+	private Action updateAction;
 	
-	public Controller(){
+	public Controller() {
 		view = new View();
 		model = new Model(view.getWidth(), view.getHeight(), view.getImageWidth(), view.getImageHeight());
+		updateAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//increment the x and y coordinates, alter direction if necessary
+				model.updateLocationAndDirection();
+				//update the view
+				view.update(model.getX(), model.getY(), model.getDirect());
+			}
+		};
 	}
 	
-        //run the simulation
-	public void start(){
-		for(int i = 0; i < 5000; i++)
-		{
-			//increment the x and y coordinates, alter direction if necessary
-			model.updateLocationAndDirection();
-			//update the view
-			view.update(model.getX(), model.getY(), model.getDirect());
-		}
+	//run the simulation
+	public void start() {
+		Timer timer = new Timer(30, updateAction);
+		timer.start();
 	}
 }
