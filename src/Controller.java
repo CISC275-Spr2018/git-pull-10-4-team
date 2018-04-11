d230175a08644496d99587938c598c4689a8742b
Matthew Zelinsky <zelinsky@udel.edu>
2018-04-11 17:34:41 -0400

    Removed fire testing code

--- src/Controller.java
+++ src/Controller.java
@@ -5,25 +5,25 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Controller implements ActionListener{
+public class Controller implements ActionListener {
 	
 	// Pull req test
 	
 	private Model model;
 	private View view;
 	private Timer updater;
-
+	
 	public Controller() {
 		view = new View();
 		model = new Model(view.getWidth(), view.getHeight(), View.getImageWidth(), View.getImageHeight());
 		view.setButtonListener(this); //add this as a listener to the button in view
 		view.setKeyListener(new DirectionKeyListener());
 		view.setKeyListener(new ActionKeyListener());
-
+		
 		initTimer();
 	}
-
-	private void initTimer(){
+	
+	private void initTimer() {
 		//define what the timer should do on each tick
 		Action updateAction = new AbstractAction() {
 			@Override
@@ -35,7 +35,7 @@ public class Controller implements ActionListener{
 				view.updateLocation(model.getX(), model.getY());
 			}
 		};
-
+		
 		//create the actual timer
 		updater = new Timer(50, updateAction);
 		
@@ -44,80 +44,80 @@ public class Controller implements ActionListener{
 	// When the button in the view is clicked
 	@Override
 	public void actionPerformed(ActionEvent e) {
-	    	if (updater.isRunning()) {
-	    		updater.stop();
-			} else {
-				updater.start();
-			}
-	    	
-	    	view.updateButton(updater.isRunning());
+		if (updater.isRunning()) {
+			updater.stop();
+		} else {
+			updater.start();
+		}
+		
+		view.updateButton(updater.isRunning());
 	}
 	
 	//run the simulation
 	public void start() {
-        EventQueue.invokeLater(new Runnable() {
+		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				updater.start();
 			}
 		});
 	}
-
+	
 	// add an instance of this as a key listener to the view
 	private class DirectionKeyListener implements KeyListener {
 		boolean pressed = false;
 		int currentKey;
 		int lastKey;
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-            if(!pressed) {
-                pressed = true;
-                currentKey = e.getKeyCode();
-                model.changeDirection(currentKey); // what happens when a key is released
-                if(!updater.isRunning() && currentKey != lastKey) {
-                    view.updateDirection(model.getDirect());
-                    lastKey = e.getKeyCode();
-                } else if (updater.isRunning()) {
-                    lastKey = 0;
-                }
-            }
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            if (e.getKeyCode() == currentKey){
-                pressed = false;
-            }
-        }
-
-        @Override
-        public void keyTyped(KeyEvent e) {
-        }
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if (!pressed) {
+				pressed = true;
+				currentKey = e.getKeyCode();
+				model.changeDirection(currentKey); // what happens when a key is released
+				if (!updater.isRunning() && currentKey != lastKey) {
+					view.updateDirection(model.getDirect());
+					lastKey = e.getKeyCode();
+				} else if (updater.isRunning()) {
+					lastKey = 0;
+				}
+			}
+		}
+		
+		@Override
+		public void keyReleased(KeyEvent e) {
+			if (e.getKeyCode() == currentKey) {
+				pressed = false;
+			}
+		}
+		
+		@Override
+		public void keyTyped(KeyEvent e) {
+		}
 	}
 	
 	// a key listener for actions such as jump and fire
-	private class ActionKeyListener implements KeyListener{
+	private class ActionKeyListener implements KeyListener {
 		boolean pressed = false;
 		int currentKey;
-
+		
 		@Override
 		public void keyPressed(KeyEvent e) {
-			if(!pressed && updater.isRunning()) {
-                pressed = true;
-                currentKey = e.getKeyCode();
-                model.doAction(currentKey);
+			if (!pressed && updater.isRunning()) {
+				pressed = true;
+				currentKey = e.getKeyCode();
+				model.doAction(currentKey);
 			}
 		}
-
+		
 		@Override
 		public void keyReleased(KeyEvent e) {
-			if (e.getKeyCode() == currentKey){
-                pressed = false;
-            }
+			if (e.getKeyCode() == currentKey) {
+				pressed = false;
+			}
 			
 		}
-
+		
 		@Override
 		public void keyTyped(KeyEvent e) {
 			
@@ -126,6 +126,6 @@ public class Controller implements ActionListener{
 		
 		
 	}
-
+	
 	
 }
--- src/View.java
+++ src/View.java
@@ -28,9 +28,11 @@ public class View extends JPanel {
     final static int frameWidth = 500*2;
     final static int frameHeight = 300*2;
 
-    private int imageNum;
+    private int imageNum = 0;
     private HashMap<Direction, Sprite> spriteMap;
     private JFrame frame;
+    
+    private int jumpImageFrame = -1; // -1 means we are not jumping, so there are no jump image frames
 
     //cached from last update call
     private int curX;
@@ -67,7 +69,7 @@ public class View extends JPanel {
     	startStopButton.setMnemonic(KeyEvent.VK_S);
     	startStopButton.setToolTipText("Click to start/stop the animation");
 
-		JLabel directionChangeLabel = new JLabel("Arrow keys to change direction");
+		JLabel directionChangeLabel = new JLabel("Arrow Keys to change direction | F to toggle fire | J to jump");
 		directionChangeLabel.setForeground(Color.white);
     	buttonPanel.add(startStopButton);
 		buttonPanel.add(directionChangeLabel);
@@ -101,13 +103,25 @@ public class View extends JPanel {
         curX = (int) x;
         curY = (int) y;
 
-        frame.repaint();
         imageNum = (imageNum + 1) % curSprite.getNumImages();
+        
+        frame.repaint();
     }
 
     public void updateDirection(Direction direct) {
-
         curSprite = Sprite.getSprite(direct);
+        if(direct.isJumping() && jumpImageFrame == -1) { // If this is the start of the jump animation, load the first frame
+        	imageNum = 0;
+        }
+        
+        if (direct.isJumping()) {	// If we're jumping, keep track of the jump frames and stop jumping when the animation is over
+        	jumpImageFrame++;
+        	if (jumpImageFrame == curSprite.getNumImages() - 1) {
+        		direct.jump();
+        		jumpImageFrame = -1;
+        	}
+        }
+        
         frame.repaint();
     }
 
